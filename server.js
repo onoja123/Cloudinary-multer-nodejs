342228c06bb3346e4f81ad58cfef964cf5204594
Okpe onoja <okpeonoja18@gmail.com>
2022-11-23 21:24:11 +0100

    first push

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+dotenv.config({path: "./config.env"});
+
+// Connect DB
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true
+  })
+  .then(() => console.log("mongoDB is connected"))
+//   .catch((err) => console.log(err));
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// Route
+app.use("/user", require("./routes/user"));
+
+app.listen(5000, () => console.log("Server is running"));
